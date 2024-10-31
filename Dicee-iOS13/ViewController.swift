38911f9b3fc4b75f9a19699ed3c06212dbf56431
Safa Can BAŞ <bassafacan@gmail.com>
2024-10-31 22:28:29 +0300

    Project Files
    Project files uploaded.

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // IBOutlet harhangi bir UI elementine refereans vermemi sağlar.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+        
+        
+    }
+    
+    
+}
+
